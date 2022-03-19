cbef6fa0bfbb8a3dc19a6d56befd7f22e1c365d6
Richard Clegg <richard.clegg@me.com>
2022-03-19 16:06:12 +0000

    Initial Commit

---
+++ Generator/GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneratorApp.swift
+//  Generator
+//
+//  Created by Richard Clegg on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
